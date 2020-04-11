d1f92cea261124ec4b245e45dd7aa86501a15359
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-11 12:54:39 +0530

    Modified runC.sh script

--- tools/runC.sh
+++ tools/runC.sh
@@ -2,11 +2,14 @@ rm -rf *.o
 echo '\n'Compiling...
 gcc -c *.c
 echo ...done
-echo '\n'linking...
-gcc -o a.out  *.o
-echo ...done
-echo '\n'executing...'\n'
-./a.out
-rm -rf *.o
-rm -rf a.out
+if(($? == 0))
+then
+  echo '\n'linking...
+  gcc -o a.out  *.o
+  echo ...done
+  echo '\n'executing...'\n'
+  ./a.out
+  rm -rf *.o
+  rm -rf a.out
 echo '\n'...done
+fi
