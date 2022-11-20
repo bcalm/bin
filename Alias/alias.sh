b211e02d8e9ed8d3d071b1cd144e3eb2b3bb26d8
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2022-11-20 14:21:55 +0530

    Added git alias

--- Alias/alias.sh
+++ Alias/alias.sh
@@ -1,6 +1,13 @@
 alias grep="grep --color=always";
-alias glo="git log";
+alias glo="git log --graph --all --oneline --decorate";
 alias gst="git status";
 alias nt="npm test";
-alias gco="git commit";
-alias gadd="git add";
+alias gc="git commit";
+alias ga="git add";
+alias gch="git checkout";
+alias gd="git diff"
+alias gm="git merge"
+alias gb="git pull rebase"
+alias stash="git stash"
+alias pop="git stash pop"
+
